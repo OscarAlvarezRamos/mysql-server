fcd4b48fb9a1f4ca6285d75bd4086cbcb3ac308c
Oscar J. Alvarez-Ramos <oscar.j.alvarez.ramos@gmail.com>
2018-05-19 12:46:34 -0400

    Startup script fixed and tested.

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,6 @@ RUN rpm -U --quiet /tmp/p7zip-plugins-16.02-10.el7.x86_64.rpm
 
 
 
-ADD temp/script.sh /docker-entrypoint-initdb.d
+# ADD temp/script.sh /docker-entrypoint-initdb.d
 
 expose 3306
---
+++ temp/init.sql
@@ -0,0 +1,4 @@
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
+
